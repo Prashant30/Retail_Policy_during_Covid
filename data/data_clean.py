1056fab59e6a12a34f8d204a3368fa6b97950879
unknown <prashantkr30@gmail.com>
2020-08-21 10:51:00 -0500

    Corrected directory for code file

--- data/data_clean.py
+++
@@ -1,6 +0,0 @@
-import csv
-import pandas as pd
-
-dataframe = pd.read_csv("D:/UTD/Codes/Hack4Peop/owid-covid-data.csv")
-new_data = dataframe[dataframe.location == 'United States']
-new_data.to_csv("D:/UTD/Codes/Hack4Peop/USData.csv")
